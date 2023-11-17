5f5ef09dec374a5bf326458757a3b27188c23ac7
Russell Gordon <rgordon@lcs.on.ca>
2023-11-17 06:25:28 -0500

    Initial Commit

---
+++ NavigationLinkExample/NavigationLinkExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationLinkExampleApp.swift
+//  NavigationLinkExample
+//
+//  Created by Russell Gordon on 2023-11-17.
+//
+
+import SwiftUI
+
+@main
+struct NavigationLinkExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
